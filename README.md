533d8e7cf27bd350061df72261e7256e7d46b2d0
zfree <79763939@qq.com>
2020-04-27 14:58:13 +0800

    编写readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# think-webhook
+此项目主要提供一个拉取即用的基于`node`的`webhook`服务项目
+
+```
+git pull
+
+npm install
+
+sh run.sh &
+```
+
+如有需要修改`run.sh`中的`log`路径和`index.js`中的端口和`handleOption`
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DATE=$(date +%Y-%m-%d-%H-%M-%S)
-nohup npm run start >> /home/logs/think-webhook/webhook.$DATE.log 2>&1
+npm run start >> /home/logs/think-webhook/webhook.$DATE.log 2>&1
